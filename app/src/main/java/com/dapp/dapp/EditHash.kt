2c2eeef5c632f9ed94a39be441f60aead54ef675
pallav dubey <rajeevpallav606@gmail.com>
2020-02-17 02:44:19 +0530

    More cleanups

--- app/src/main/java/com/dapp/dapp/EditHash.kt
+++ app/src/main/java/com/dapp/dapp/EditHash.kt
@@ -1,9 +1,10 @@
 package com.dapp.dapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.dapp.dapp.blockHelper.Block
 import com.dapp.dapp.helper.PrefManager
 import com.google.firebase.database.FirebaseDatabase
 
@@ -16,6 +17,16 @@ class EditHash : AppCompatActivity() {
         val editText:EditText=findViewById(R.id.hash)
         val submit:Button=findViewById(R.id.submit)
         submit.setOnClickListener {
+            if (editText.text.toString().trim().isNotEmpty()) {
+                MainApplication.blockChain.addBlock(
+                    Block(
+                        MainApplication.currentHash.value.toString(),
+                        editText.text.toString(),
+                        "Yo!",
+                        "Someone"
+                    )
+                )
+            }
             MainApplication.currentHash.value=editText.text.toString()
             FirebaseDatabase.getInstance()
                 .getReference(PrefManager.getString("phone","somevalue")).setValue(editText.text.toString())
