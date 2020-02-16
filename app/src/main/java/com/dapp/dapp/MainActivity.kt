pallav12/DApp

added viewpager

app/src/main/java/com/dapp/dapp/MainActivity.kt
@@ -1,12 +1,74 @@
 package com.dapp.dapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = findViewById(R.id.pager)
+
+        // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
     }
+
+    override fun onBackPressed() {
+        if (mPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            mPager.currentItem = mPager.currentItem - 1
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : PagerAdapter() {
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            return view == `object`
+            //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getCount(): Int {
+            return 5 //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val layoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val view = layoutInflater!!.inflate(R.layout.fragment_viewpager, container, false)
+            val textView: TextView = view.findViewById(R.id.tv_viewpager);
+            textView.setText("" + position)
+            container.addView(view)
+            return view
+
+            return super.instantiateItem(container, position)
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            if (`object` is View) container.removeView(`object`)
+        }
+    }
+
+
 }

technex-iit-bhu/Appathon20

app/src/main/java/com/dapp/dapp/MainActivity.kt
@@ -1,12 +1,79 @@
 package com.dapp.dapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.dapp.dapp.helper.PrefManager
+import com.dapp.dapp.home.GroupListingActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (!PrefManager.getBoolean("first_time", true)) {
+            val intent = Intent(this, GroupListingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        mPager = findViewById(R.id.pager)
+
+        // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
     }
+
+    override fun onBackPressed() {
+        if (mPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            mPager.currentItem = mPager.currentItem - 1
+        }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : PagerAdapter() {
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            return view == `object`
+        }
+
+        override fun getCount(): Int {
+            return 5
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val layoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val view = layoutInflater.inflate(R.layout.fragment_viewpager, container, false)
+
+            val textView: TextView = view.findViewById(R.id.tv_viewpager)
+            if (position == 4) {
+                val intent = Intent(view.context, GroupListingActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            textView.setText("" + position)
+            container.addView(view)
+            return view
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            if (`object` is View) container.removeView(`object`)
+        }
+    }
+
+
 }